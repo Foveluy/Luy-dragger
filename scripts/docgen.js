32b832d4a3bf7177fa0720ddb938209817159e3d
方正 <215566435@qq.com>
2018-08-14 00:24:14 +0800

    fix: bugs

--- scripts/docgen.js
+++ scripts/docgen.js
@@ -11,6 +11,6 @@ const readme = fs.readFileSync('./scripts/readme.md', 'utf8');
 fs.writeFileSync('./README.md', readme + '\n' + md.md);
 
 const indexHtml = fs.readFileSync('./docs/index.html', 'utf8');
-const newHtml = indexHtml.replace('/static/css', '').replace('/static/js', '');
+const newHtml = indexHtml.replace('/static/css', '.').replace('/static/js', '.');
 
 fs.writeFileSync('./docs/index.html', newHtml);
