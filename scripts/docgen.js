2f485740b68af0b50421eee8f9eefe1ae5539d69
方正 <215566435@qq.com>
2018-08-14 00:19:42 +0800

    chore: 全自动生成文档

--- scripts/docgen.js
+++ scripts/docgen.js
@@ -6,7 +6,11 @@ const file = fs.readFileSync('./src/Dragger/index.js', 'utf8');
 const json = ReactComponentJson.json(file);
 const md = ReactComponentJson.md(json);
 
-
 const readme = fs.readFileSync('./scripts/readme.md', 'utf8');
 
-fs.writeFileSync('./README.md', readme + "\n" + md.md);
+fs.writeFileSync('./README.md', readme + '\n' + md.md);
+
+const indexHtml = fs.readFileSync('./docs/index.html', 'utf8');
+const newHtml = indexHtml.replace('/static/css', '').replace('/static/js', '');
+
+fs.writeFileSync('./docs/index.html', newHtml);
